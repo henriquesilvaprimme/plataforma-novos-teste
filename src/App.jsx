c1f32d5feccfab1d82623001b8f5e21389b28685
henriquesilvaprimme <henriquesilva.primme@gmail.com>
2025-11-20 14:37:17 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ function ScrollToTop({ scrollContainerRef }) {
   return null;
 }
 
+const GOOGLE_APPS_SCRIPT_BASE_URL = '/api/gas';
 const GOOGLE_APPS_SCRIPT_BASE_URL = 'https://script.google.com/macros/s/AKfycbzSkLIDEJUeJMf8cQestU8jVAaafHPPStvYsnsJMbgoNyEXHkmz4eXica0UOEdUQFea/exec';
 const GOOGLE_SHEETS_SCRIPT_URL = `${GOOGLE_APPS_SCRIPT_BASE_URL}?v=getLeads`;
 const GOOGLE_SHEETS_LEADS_FECHADOS = `${GOOGLE_APPS_SCRIPT_BASE_URL}?v=pegar_clientes_fechados`;
