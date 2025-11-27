c334ebb65866309ac64faaac6a0185563ecee427
henriquesilvaprimme <henriquesilva.primme@gmail.com>
2025-11-27 19:06:07 -0300

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import { collection, onSnapshot } from 'firebase/firestore';
-import { db } from './firebase';
+import { db } from '../firebase';
 import { RefreshCcw } from 'lucide-react'; // Importação do ícone de refresh
 
 const Dashboard = ({ usuarioLogado }) => {
